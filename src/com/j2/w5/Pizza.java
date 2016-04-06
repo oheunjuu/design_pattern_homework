31fd8a4e1ca266e8d96cea02bc4488c0df7a436c
oheunjuu <essenti10@naver.com>
2016-04-06 18:00:36 +0900

    20160406_pizza making

---
+++ src/com/j2/w5/Pizza.java
@@ -0,0 +1,17 @@
+package com.j2.w5;
+
+abstract public class Pizza{
+  String name;
+  public void prepare(){
+    System.out.println("preparing "+name);
+  }
+  public void bake(){
+    System.out.println("baking "+name);
+  }
+  public void cut(){
+    System.out.println("cutting "+name);
+  }
+  public void box(){
+    System.out.println("boxing "+name);
+  }
+}
---
+++ src/com/j2/w5/PizzaTestDrive.java
@@ -0,0 +1,10 @@
+package com.j2.w5;
+
+public class PizzaTestDrive {
+  public static void main(String[] args){
+    SimplePizzaFactory factory = new SimplePizzaFactory();
+    PizzaStore store = new PizzaStore(factory);
+    Pizza pizza=store.orderPizza("cheese");
+    Pizza pizza2=store.orderPizza("veggie");
+  }
+}
